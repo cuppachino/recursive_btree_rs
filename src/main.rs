f6e0403c515fe4d3c2281548075c2c56f9e37760
cuppachino <cuppachino.dev@gmail.com>
2023-03-04 05:20:27 -0600

    tsodings non-recursive approach

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,47 @@ fn generate_tree(level: usize, counter: &mut i32) -> NodeRef<i32> {
     }
 }
 
-fn print_tree<T: Display>(root: &NodeRef<T>, level: usize) {
-    match root {
-        Some(node) => {
-            print_tree(&node.right, level + 1);
-            println!("{}{}", "   ".repeat(level as usize), node.value);
-            print_tree(&node.left, level + 1);
+fn visit_nodes<T: Display>(root: &NodeRef<T>) {
+    if let Some(node) = root {
+        println!("{}", node.value);
+        visit_nodes(&node.left);
+        visit_nodes(&node.right);
+    }
+}
+
+enum Action<T, U> {
+    Call(T),
+    Handle(U),
+}
+
+fn print_tree_nonrec<T: Display>(root: &NodeRef<T>) {
+    let mut stack = Vec::<Action<(&NodeRef<T>, usize), (&T, usize)>>::new();
+    use Action::*;
+    stack.push(Call((root, 0)));
+    while let Some(action) = stack.pop() {
+        match action {
+            Call((root, level)) => {
+                if let Some(node) = root {
+                    stack.push(Call((&node.left, level + 1)));
+                    stack.push(Handle((&node.value, level)));
+                    stack.push(Call((&node.right, level + 1)));
+                }
+            }
+            Handle((value, level)) => {
+                for _ in 0..level {
+                    print!("  ");
+                }
+                println!("{}", value);
+            }
         }
-        None => {}
+    }
+}
+
+fn print_tree<T: Display>(root: &NodeRef<T>, level: usize) {
+    if let Some(node) = root {
+        print_tree(&node.right, level + 1);
+        println!("{}{}", "  ".repeat(level as usize), node.value);
+        print_tree(&node.left, level + 1);
     }
 }
 
@@ -50,8 +83,17 @@ fn invert_tree<T: Clone>(root: &NodeRef<T>) -> NodeRef<T> {
 fn main() {
     let mut counter = 1;
     let tree = generate_tree(3, &mut counter);
-    print_tree(&tree, 0);
 
+    print_tree_nonrec(&tree);
+    println!("{}", "-".repeat(8));
+
+    print_tree_nonrec(&invert_tree(&tree));
+    println!("{}", "-".repeat(8));
+
+    visit_nodes(&tree);
+    println!("{}", "-".repeat(8));
+
+    print_tree(&tree, 0);
     println!("{}", "-".repeat(8));
 
     let inverted = invert_tree(&tree);
